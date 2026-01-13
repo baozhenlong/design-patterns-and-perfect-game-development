76297d462f0c911b81f0cb74c284e54445bde86d
EmotionalDamon <435801541@qq.com>
2026-01-13 15:23:58 +0800

    del: 多余文件

--- design-patterns-1/Assets/AbstractFactory/AbstractFactory.cs
+++
@@ -1,9 +0,0 @@
-namespace GoF.AbstractFactory
-{
-    // 可生成各抽象成品对象的操作
-    public abstract class AbstractFactory
-    {
-        public abstract AbstractProductA CreateProductA();
-        public abstract AbstractProductB CreateProductB();
-    }
-}
